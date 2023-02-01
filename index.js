09005875dcf69cb86a3a23004947e7f7175c6d55
unknown <martinwangata@gmail.com>
2023-02-01 12:45:00 +0300

    Wrote code to display data on the frontend from the backend using cors

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 const PORT = 8000
-const axios = require('axios');
-const cheerio = require('cheerio');
-const express = require('express');
-
+const axios = require('axios')
+const cheerio = require('cheerio')
+const express = require('express')
+const cors = require('cors')
 const app = express()
+app.use(cors())
 
-const url = 'https://nation.africa/kenya'
 
-axios(url)
+
+const url = 'https://www.theguardian.com/uk'
+
+// app.METHOD(PATH, HANDLER)
+
+app.get('/', function (req, res) {
+    res.json('This is my webscrapper')
+})
+
+
+app.get('/results', (req, res) => {
+    axios(url)
     .then(response => {
         const html =response.data
         const $ = cheerio.load(html)
         const articles = []
-        $('.headline-teasers_item', html).each(function() {
+        $('.fc-item__title', html).each(function() {
             const title = $(this).text()
             const url = $(this).find('a').attr('href')
             articles.push({
@@ -20,8 +31,11 @@ axios(url)
                 url
             })
         })
-        console.log(articles)
+        res.json(articles)
     }).catch(err => console.log(err))
+})
+
+
 
 
 app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`))
